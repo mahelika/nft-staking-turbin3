f5596b11e2b289195633e8353c168a592294de45
mahelikaa <imahelika@gmail.com>
2025-04-23 03:38:47 +0530

    initial commit

---
+++ programs/nft-staking/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("D37tSyjmXxmvA6r8uBQ7T7qLcf6d8Fo5rmZNfDd3UiSn");
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
